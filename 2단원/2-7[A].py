71ec5008ed03cb9e194be9bc969261d7dad127e9
so528 <riseyoo5@naver.com>
2024-07-05 15:28:43 +0900

    2-7[A].py

--- 2단원/2-7[A].py
+++ 2단원/2-7[A].py
@@ -1,6 +1,6 @@
 # 10진수 정숫값을 입력받아 2~36진수로 변환하여 출력하기
 
-def card_conv(x: int, r:int):
+def card_conv(x: int, r:int) -> str:
     d=''
     dchar = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     while x > 0:

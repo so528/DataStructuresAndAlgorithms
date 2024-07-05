d28b8831347fe4c166f5f14ec5055db0fc2776c1
so528 <riseyoo5@naver.com>
2024-07-05 13:12:05 +0900

    Add files via upload

---
+++ 2단원/2-5.py
@@ -0,0 +1,13 @@
+# 각 배열 원소의 최댓값을 구해서 출력하기(튜플, 문자열, 문자열 리스트)
+
+from max import max_of
+
+t=(1,2,3,4,5,)
+
+s='string'
+
+a=['DTS','AAC','FLAC',]
+
+print(f'{t}의 최댓값은 {max_of(t)}입니다.')
+print(f'{s}의 최댓값은 {max_of(s)}입니다.')
+print(f'{a}의 최댓값은 {max_of(a)}입니다.')
